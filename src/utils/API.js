e63136ea7c660a6704578eff543c0e3d730d6eb2
giorgi <giorgi199817@gmail.com>
2019-09-08 03:53:28 +0400

    Modified api url

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost/gsp/gsp_api/api/",
+  baseURL: "https://master.d1ubo1y2yglotf.amplifyapp.com/gsp/gsp_api/api/",
   responseType: "json"
 });
