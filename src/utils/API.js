4836b1a0c54b7fa7b6022700a3f5728535f1c35c
giorgi <giorgi199817@gmail.com>
2019-09-08 04:17:38 +0400

    Modified api url

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://master.d1ubo1y2yglotf.amplifyapp.com/gsp/gsp_api/api/",
+  // baseURL: "https://master.d1ubo1y2yglotf.amplifyapp.com/gsp/gsp_api/api/",
+  baseURL: "/gsp/gsp_api/api/",
   responseType: "json"
 });
