018f347580490a668ed8f85f61a448ac7cfc44e3
giorgi17 <giorgi199817@gmail.com>
2019-10-11 19:35:14 +0400

    api url modified

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://ec2-18-224-139-141.us-east-2.compute.amazonaws.com:443/gsp/gsp_api/api/",
+  baseURL: "http://ec2-18-224-139-141.us-east-2.compute.amazonaws.com:443/",
   responseType: "json"
 });
