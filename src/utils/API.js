6458522f42d444ad441821213461c8375be559c6
giorgi17 <giorgi199817@gmail.com>
2019-10-17 04:53:02 +0400

    New url for api

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://ec2-18-224-139-141.us-east-2.compute.amazonaws.com:443/",
+  baseURL: "https://ec2-18-224-139-141.us-east-2.compute.amazonaws.com/",
   responseType: "json"
 });
