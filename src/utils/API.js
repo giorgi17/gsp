35d29098cc706f3ee3cc106da55586b33d6dac42
giorgi17 <giorgi199817@gmail.com>
2019-11-22 09:34:49 +0400

    eh

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://ec2-18-224-139-141.us-east-2.compute.amazonaws.com:1717/",
+  baseURL: "https://ratomeseuxeshad.tk/",
   responseType: "json"
 });
